c9090990cf13422770d24fd2923aa3be83b28b87
FurkanMDemiray <furkanmdemiray@gmail.com>
2024-11-13 13:28:06 +0300

    added injection framework

--- TimePad/App/AppDelegate.swift
+++ TimePad/App/AppDelegate.swift
@@ -5,62 +5,69 @@
 //  Created by Melik Demiray on 24.09.2024.
 //
 
-import UIKit
 import CoreData
+import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    // Override point for customization after application launch.
 
+    return true
+  }
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+  // MARK: UISceneSession Lifecycle
 
-    // MARK: UISceneSession Lifecycle
+  func application(
+    _ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession,
+    options: UIScene.ConnectionOptions
+  ) -> UISceneConfiguration {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return UISceneConfiguration(
+      name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
+  func application(
+    _ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+  ) {
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+  }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+  // MARK: - Core Data stack
 
-    // MARK: - Core Data stack
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
+  lazy var persistentContainer: NSPersistentContainer = {
+    /*
            Core Data için kalıcı veri saklama konteyneri. 'DataModel' adında bir veritabanı oluşturur.
            */
-        let container = NSPersistentContainer(name: "DataModel") // DataModel, veritabanı modelinizin ismi olmalı
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Hata durumunda uygulamayı sonlandırabilirsiniz.
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
+    let container = NSPersistentContainer(name: "DataModel")  // DataModel, veritabanı modelinizin ismi olmalı
+    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+      if let error = error as NSError? {
+        // Hata durumunda uygulamayı sonlandırabilirsiniz.
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    })
+    return container
+  }()
 
-    // MARK: - Core Data Saving support
+  // MARK: - Core Data Saving support
 
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
+  func saveContext() {
+    let context = persistentContainer.viewContext
+    if context.hasChanges {
+      do {
+        try context.save()
+      } catch {
+        let nserror = error as NSError
+        fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+      }
     }
+  }
 
 }
-
--- TimePad/Screens/Home/HomeVC.swift
+++ TimePad/Screens/Home/HomeVC.swift
@@ -9,157 +9,164 @@ import UIKit
 
 final class HomeVC: UIViewController {
 
-//MARK: Outlets
-    @IBOutlet private weak var timerCardView: UIView!
-    @IBOutlet private weak var timerLabel: UILabel!
-    @IBOutlet private weak var projectLabel: UILabel!
-    @IBOutlet private weak var tableView: UITableView!
-    @IBOutlet private weak var seeAllLabel: UILabel!
-    @IBOutlet private weak var playImage: UIImageView!
-    @IBOutlet private weak var emptyDataLabel: UILabel!
+  //MARK: Outlets
+  @IBOutlet private weak var timerCardView: UIView!
+  @IBOutlet private weak var timerLabel: UILabel!
+  @IBOutlet private weak var projectLabel: UILabel!
+  @IBOutlet private weak var tableView: UITableView!
+  @IBOutlet private weak var seeAllLabel: UILabel!
+  @IBOutlet private weak var playImage: UIImageView!
+  @IBOutlet private weak var emptyDataLabel: UILabel!
 
-    var viewModel: HomeVMProtocol! {
-        didSet {
-            viewModel.delegate = self
-        }
+  var viewModel: HomeVMProtocol! {
+    didSet {
+      viewModel.delegate = self
     }
+  }
 
-//MARK: Lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupConfigures()
+  //MARK: Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupConfigures()
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    viewModel.didFetchWorkModels()
+    viewModel.didFetchLastWork()
+    //setInnerOfCardView()
+  }
+
+  //MARK: Configures
+  private func configureUI() {
+    timerCardView.layer.cornerRadius = 10
+    timerCardView.layer.shadowColor = UIColor.black.cgColor
+    timerCardView.layer.shadowOffset = CGSize(width: 0, height: 1)
+    timerCardView.layer.shadowOpacity = 0.2
+    timerCardView.layer.shadowRadius = 2
+
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(seeAllTapped))
+    seeAllLabel.isUserInteractionEnabled = true
+    seeAllLabel.addGestureRecognizer(tapGesture)
+  }
+
+  private func configureTableView() {
+    tableView.delegate = self
+    tableView.dataSource = self
+    tableView.separatorStyle = .none
+    tableView.register(
+      UINib(nibName: Constants.projectCell, bundle: nil),
+      forCellReuseIdentifier: Constants.projectCell)
+  }
+
+  private func setInnerOfCardView() {
+    if viewModel.getFinishString != "" {
+      projectLabel.text = viewModel.getFinishString
+    } else {
+      projectLabel.text = viewModel.getLastWork.title
     }
+    timerLabel.text = viewModel.getLastWorkTime
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        viewModel.didFetchWorkModels()
-        viewModel.didFetchLastWork()
-        setInnerOfCardView()
+  }
+
+  private func configureInnerOfCardView() {
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(timerCardTapped))
+    timerCardView.isUserInteractionEnabled = true
+    timerCardView.addGestureRecognizer(tapGesture)
+  }
+
+  private func setupConfigures() {
+    configureUI()
+    configureTableView()
+  }
+
+  //MARK: Navigate
+  private func navigateToTimerVC(at index: Int) {
+    let timerVC = TimerVC()
+    timerVC.workModel = viewModel.getWorkModels[index]
+    navigationController?.pushViewController(timerVC, animated: true)
+  }
+
+  //MARK: - Actions
+  @objc private func seeAllTapped() {
+    print("See all tapped")
+  }
+
+  @objc private func timerCardTapped() {
+    // Find last work depending on title
+    guard let lastWorkTitle = projectLabel.text,
+      let lastWorkTime = timerLabel.text
+    else { return }
+
+    // Find index of last work model depending on title and time
+    if let index = viewModel.getWorkModels.firstIndex(where: {
+      $0.title == lastWorkTitle && viewModel.getLastWorkTime == lastWorkTime
+    }) {
+      navigateToTimerVC(at: index)
     }
-
-//MARK: Configures
-    private func configureUI() {
-        timerCardView.layer.cornerRadius = 10
-        timerCardView.layer.shadowColor = UIColor.black.cgColor
-        timerCardView.layer.shadowOffset = CGSize(width: 0, height: 1)
-        timerCardView.layer.shadowOpacity = 0.2
-        timerCardView.layer.shadowRadius = 2
-
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(seeAllTapped))
-        seeAllLabel.isUserInteractionEnabled = true
-        seeAllLabel.addGestureRecognizer(tapGesture)
-    }
-
-    private func configureTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.separatorStyle = .none
-        tableView.register(UINib(nibName: Constants.projectCell, bundle: nil), forCellReuseIdentifier: Constants.projectCell)
-    }
-
-    private func setInnerOfCardView() {
-        if viewModel.getFinishString != "" {
-            projectLabel.text = viewModel.getFinishString
-        } else {
-            projectLabel.text = viewModel.getLastWork.title
-        }
-        timerLabel.text = viewModel.getLastWorkTime
-
-    }
-
-    private func configureInnerOfCardView() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(timerCardTapped))
-        timerCardView.isUserInteractionEnabled = true
-        timerCardView.addGestureRecognizer(tapGesture)
-    }
-
-    private func setupConfigures() {
-        configureUI()
-        configureTableView()
-    }
-
-    //MARK: Navigate
-    private func navigateToTimerVC(at index: Int) {
-        let timerVC = TimerVC()
-        timerVC.workModel = viewModel.getWorkModels[index]
-        navigationController?.pushViewController(timerVC, animated: true)
-    }
-
-    //MARK: - Actions
-    @objc private func seeAllTapped() {
-        print("See all tapped")
-    }
-
-    @objc private func timerCardTapped() {
-        // Find last work depending on title
-        guard let lastWorkTitle = projectLabel.text,
-            let lastWorkTime = timerLabel.text else { return }
-
-        // Find index of last work model depending on title and time
-        if let index = viewModel.getWorkModels.firstIndex(where: {
-            $0.title == lastWorkTitle && viewModel.getLastWorkTime == lastWorkTime
-        }) {
-            navigateToTimerVC(at: index)
-        }
-    }
+  }
 }
 
 //MARK: - TableViewDelegate
 extension HomeVC: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.getWorkModels.count
-    }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.getWorkModels.count
+  }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.projectCell, for: indexPath) as! ProjectCell
-        cell.configure(with: viewModel.getWorkModels[indexPath.row])
-        return cell
-    }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell =
+      tableView.dequeueReusableCell(withIdentifier: Constants.projectCell, for: indexPath)
+      as! ProjectCell
+    cell.configure(with: viewModel.getWorkModels[indexPath.row])
+    return cell
+  }
 
-    // delete cell
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            self.showAreYouSureAlert(
-                title: Constants.areYouSureAlertActionTitle,
-                message: Constants.areYouSureAlertMessage,
-                actionTitle: Constants.areYouSureAlertActionTitle,
-                completion: { [weak self] in
-                    guard let self else { return }
-                    viewModel.deleteWordModel(at: indexPath.row)
-                })
-        }
-        viewModel.didFetchWorkModels()
+  // delete cell
+  func tableView(
+    _ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle,
+    forRowAt indexPath: IndexPath
+  ) {
+    if editingStyle == .delete {
+      self.showAreYouSureAlert(
+        title: Constants.areYouSureAlertActionTitle,
+        message: Constants.areYouSureAlertMessage,
+        actionTitle: Constants.areYouSureAlertActionTitle,
+        completion: { [weak self] in
+          guard let self else { return }
+          viewModel.deleteWordModel(at: indexPath.row)
+        })
     }
+    viewModel.didFetchWorkModels()
+  }
 
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        if viewModel.getWorkModels.count == 4 {
-//            return tableView.frame.height / CGFloat(self.tableView(tableView, numberOfRowsInSection: 0))
-//        } else {
-//            return tableView.frame.height / 4
-//        }
-//    }
+  //    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+  //        if viewModel.getWorkModels.count == 4 {
+  //            return tableView.frame.height / CGFloat(self.tableView(tableView, numberOfRowsInSection: 0))
+  //        } else {
+  //            return tableView.frame.height / 4
+  //        }
+  //    }
 
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        navigateToTimerVC(at: indexPath.row)
-    }
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    navigateToTimerVC(at: indexPath.row)
+  }
 }
 //MARK: - HomeVMDelegate
 extension HomeVC: HomeVMDelegate {
-    func updateTimerCard() {
-        configureInnerOfCardView()
-    }
+  func updateTimerCard() {
+    configureInnerOfCardView()
+  }
 
-    func showEmptyView() {
-        emptyDataLabel.isHidden = false
-    }
+  func showEmptyView() {
+    emptyDataLabel.isHidden = false
+  }
 
-    func hideEmptyView() {
-        emptyDataLabel.isHidden = true
-    }
+  func hideEmptyView() {
+    emptyDataLabel.isHidden = true
+  }
 
-    func updateTableView() {
-        tableView.reloadData()
-        setInnerOfCardView()
-    }
+  func updateTableView() {
+    tableView.reloadData()
+    setInnerOfCardView()
+  }
 }
-
